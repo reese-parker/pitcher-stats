c5f6994ccc9c2578b5b00ff07b8ba7d1aa1ac6a2
Reese <reese@silverorange.com>
2023-01-13 23:44:21 -0500

    Create stub pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { Pitcher } from './components/Pitcher/Pitcher';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Pitcher Stats</h1>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact component={() => <Home />} path='/' />
+        <Route exact component={() => <Pitcher />} path='/pitcher' />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Pitcher/Pitcher.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Pitcher() {
+  return (
+    <div>
+      <h1>Pitcher</h1>
+    </div>
+  );
+}
